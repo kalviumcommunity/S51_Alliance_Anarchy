fef20d0f2c764bd63c59ae58493839af9ea3568c
Kishore <ki225ku@gmail.com>
2024-02-20 15:30:24 +0530

    connected database to server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { connectToDB, disconnectFromDB, isConnected } = require('./database');
+
+
 
 app.get("/",(req, res) => {
-    res.send("Hello :)")
+    connectToDB();
+    res.send(isConnected ? "Connected" : "Disconnected")
 })
 
 app.get("/ping",(req, res) => {
@@ -11,5 +15,6 @@ app.get("/ping",(req, res) => {
 })
 
 app.listen(port, () => {
+    connectToDB();
     console.log(`Server listening on port ${port}`)
 })
